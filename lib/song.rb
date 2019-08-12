3decad1db22a66b67376d23c1a3ddf9a90fb5a1a
Brian Wong <bwong92@gmail.com>
2019-08-12 00:49:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,15 +15,15 @@ class Song
     @@genres << self.genre
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def artists
+  def self.artists
     @@artists
   end
 
-  def genres
+  def self.genres
     @@genres
   end
 end
